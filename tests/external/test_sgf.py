f148db8db65856cbe9517b215c9cd11b5e4d13f3
Caleb George <koriantor@gmail.com>
2020-08-04 18:59:11 -0400

    cleanup

--- tests/external/test_sgf.py
+++ tests/external/test_sgf.py
@@ -14,33 +14,5 @@ class TestSGF(unittest.TestCase):
             game = sgf.Sgf_game.from_bytes(f.read())
         # If no crash, then test succeeds
 
-    def test_
-
-
 if __name__ == '__main__':
     unittest.main()
-
-"""Sgfmill represents Go colours and moves as follows:
-Name 	Possible values
-colour 	single-character string: 'b' or 'w'
-point 	pair (int, int) of coordinates
-move 	point or None (for a pass)
-
-The terms colour, point, and move are used as above throughout this documentation (in particular, when describing parameters and return types).
-
-colour values are used to represent players, as well as stones on the board. (When a way to represent an empty point is needed, None is used.)
-
-point values are treated as (row, column). The bottom left is (0, 0) (the same orientation as GTP, but not SGF). So the coordinates for a 9x9 board are as follows:
-
-9 (8,0)  .  .  .  .  .  (8,8)
-8  .  .  .  .  .  .  .  .  .
-7  .  .  .  .  .  .  .  .  .
-6  .  .  .  .  .  .  .  .  .
-5  .  .  .  .  .  .  .  .  .
-4  .  .  .  .  .  .  .  .  .
-3  .  .  .  .  .  .  .  .  .
-2  .  .  .  .  .  .  .  .  .
-1 (0,0)  .  .  .  .  .  (0,8)
-   A  B  C  D  E  F  G  H  J
-
-"""
